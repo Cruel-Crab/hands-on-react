d9f8f765d5b535eae72d0158780e31bff3049332
Mohammed <61859337+Cruel-Crab@users.noreply.github.com>
2024-03-16 18:08:27 +0000

    Modified function App()

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 function App() {
+  const name = 'StarGazers';
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+    <div className="container">
+      <article>
+        
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers group" />
+        <h1>
+          Meet the <span style={{ color: "salmon", fontStyle: "italic" }}>{name}</span>
+        </h1>
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <button type="button" className="outline"
+          onClick={() => {
+            alert('Clicked')
+          }}>Click here!</button>
+      </hgroup>
+      </article>
+    </div>
+  );
 }
 export default App
